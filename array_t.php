ce7a9b3a94b36e9cd5d083bab4e8d7cdbb33e7f9
Ram <ram.tiwari@mobilepedia.in>
2016-03-06 17:31:11 +0530

    initalize

---
+++ array_t.php
@@ -0,0 +1,68 @@
+<?php
+$test_array=array();
+
+$test_array['state_wise']['0']['2010']['state_data']='1';
+$test_array['state_wise']['0']['2010']['state_name']='Delhi';
+$test_array['state_wise']['0']['2011']['state_data']='3';
+$test_array['state_wise']['0']['2011']['state_name']='Delhi';
+$test_array['state_wise']['0']['2012']['state_data']='4';
+$test_array['state_wise']['0']['2012']['state_name']='Delhi';
+$test_array['state_wise']['1']['2010']['state_data']='1';
+$test_array['state_wise']['1']['2010']['state_name']='Pune';
+$test_array['state_wise']['1']['2011']['state_data']='6';
+$test_array['state_wise']['1']['2011']['state_name']='Pune';
+$test_array['state_wise']['1']['2012']['state_data']='7';
+$test_array['state_wise']['1']['2012']['state_name']='Pune';
+/*$test_array['0']['0']['Address']='New Delhi1';
+$test_array['0']['0']['MObile']='7';
+$test_array['0']['0']['Profile']='web';
+$test_array['0']['1']['name']='Mohan';
+$test_array['0']['1']['Address']='New Delhi2';
+$test_array['0']['1']['MObile']='8';
+$test_array['0']['1']['Profile']='web1';
+$test_array['1']['0']['name']='Prateek';
+$test_array['1']['0']['Address']='New Delhi1';
+$test_array['1']['0']['MObile']='7';
+$test_array['1']['0']['Profile']='web';
+$test_array['1']['1']['name']='Mohan';
+$test_array['1']['1']['Address']='New Delhi2';
+$test_array['1']['1']['MObile']='8';
+$test_array['1']['1']['Profile']='web1';*/
+
+for($i=0;$i<count($test_array['state_wise']);$i++){
+	$state_data=0;
+	$state_name='';
+	//echo " i".$i."<br/>-----------";
+	for($j=2010;$j<2013;$j++){
+		//echo $i." j".$j."<br/>";
+		$state_data+=$test_array['state_wise'][$i][$j]['state_data'];
+		$state_name=$test_array['state_wise'][$i][$j]['state_name'];
+		
+		//echo " state_data".$state_data."<br/>";
+	}
+	echo $state_data;
+	echo $state_name."----------<br>";
+}
+//var_dump($test_array);
+
+//echo $test_array[''][1]['name'];
+
+ 
+  function test($a,$b)
+  {
+	$a;
+	$b;
+	$c=$a+$b;
+	
+	//echo $c;
+	
+	$x;
+	$y;
+	$z=$x-$y;
+	//echo $z;
+	
+	
+  }
+  
+  
+?>
---
+++ drop.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+</head>
+<body>
+ 
+<form name="f1" method="post">
+    <input type="radio" name="others"  >Others
+
+    <select id="that_select">
+        <option value="a">A</option>
+        <option value="b" >B</option>
+        <option value="c">C</option>
+    </select>
+</form
+></body>
+</html>
